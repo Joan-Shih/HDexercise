cfbc3327ec7c189d6c1c1e1be0e8743a1ddb0d03
Joan Shih <b0955679421@gmail.com>
2024-06-04 16:50:57 +0800

    reset local env and chane database to Aiven

--- connection.py
+++ connection.py
@@ -1,15 +1,23 @@
-import mysql.connector
-from mysql.connector import Error
+import pymysql
 from config import settings
-from icecream import ic
+from mysql.connector import Error
 
 def connect_mysql():
+    timeout = 10
     try:
-        conn = mysql.connector.connect(host=settings.database_hostname,
-                                       database=settings.database_name,
-                                       user=settings.database_username,
-                                       password=settings.database_password)
-        cursor = conn.cursor(dictionary=True)
+        conn = pymysql.connect(
+           charset="utf8mb4",
+           connect_timeout=timeout,
+           cursorclass=pymysql.cursors.DictCursor,
+           db=settings.database_name,
+           host=settings.database_hostname,
+           password=settings.database_password,
+           read_timeout=timeout,
+           port=21732,
+           user=settings.database_username,
+           write_timeout=timeout
+           )
+        cursor = conn.cursor() #dictionary=True
         yield cursor
     except Error as e:
         print(e)
